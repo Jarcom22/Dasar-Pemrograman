e8eb1db678ba669bddae76d1e61c53df7c10cf92
Jarcom <m.fajarmuhaimin_ti24@nusaputra.ac.id>
2025-06-01 14:25:54 +0700

    Add files via upload

---
+++ Sesi 12/manipulasi.py
@@ -0,0 +1,26 @@
+import pandas as pd
+file_path = r"D:\SEMESTER 2\Dasar Pemrograman\Sesi 12\data_penjualan.xlsx"
+data = pd.read_excel(file_path, engine="openpyxl")
+
+print(data.head())
+
+#2
+data['Total Harga'] = data['Jumlah'] * data["Harga Satuan"]
+print('\nData dengan kolom Total Harga: ')
+print(data.head())
+
+#3
+data_elektronik = data[data['Kategori'] == 'Elektronik']
+data_elektronik.to_excel('elektronik.xlsx', index=False)
+print("\nData elektronik di simpan di elektronik.xlsx")
+
+#4
+rekap = data.groupby('Kategori')['Total Harga'].sum().reset_index()
+rekap.columns = ['Kategori', 'Total Pendapatan']
+print("\nRekap total pendapatan per kategori: ")
+print(rekap)
+
+#5
+data_sorted = data.sort_values(by='Total Harga', ascending=False)
+data_sorted.to_excel('penjualan_terurut.xlsx', index=False)
+print("\nData telah disimpan berdasarkan Total Harga dan disimpan di penjuaalan: ")
